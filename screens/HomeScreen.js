5cbd1ed7a8ab53e2a8346ddb42b1a03e26bce4e8
Zhannur <jannuraidynuly@gmail.com>
2022-02-07 15:47:06 +0500

    Show logged in user's email

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,11 @@
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import React from 'react';
+import { auth } from '../firebase';
 
 const HomeScreen = () => {
   return (
     <View style={styles.container}>
-      <Text>Email: </Text>
+      <Text>Email: {auth.currentUser?.email}</Text>
       <TouchableOpacity
           style={styles.button}
       >
